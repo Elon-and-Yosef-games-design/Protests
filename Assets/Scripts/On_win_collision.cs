55c5d389ffa5cb90a6612dc67d3bcf905214b24b
elonezra <elonezra1995@hotmail.com>
2023-04-04 12:13:07 +0300

    update 4
    - finished win senerio
    - finished die senerio
    - added start animation

--- Assets/Scripts/On_win_collision.cs
+++ Assets/Scripts/On_win_collision.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class On_win_collision : MonoBehaviour
 {
+    [SerializeField]
+    string win_tag;
+    [SerializeField]
+    string win_name_screen;
 
+    [SerializeField]
+    float wait_time = 0.5f;
+
+
+    IEnumerator wait_for_a_momnet()
+    {
+        yield return new WaitForSeconds(wait_time);
+        SceneManager.LoadScene(win_name_screen);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        if(collision.tag == win_tag && enabled)
+        {
+            StartCoroutine(wait_for_a_momnet());
+        }
+    }
 }
--- Assets/Scripts/collision_distroy.cs
+++ Assets/Scripts/collision_distroy.cs
@@ -1,16 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class collision_distroy : MonoBehaviour
 {
 
     [SerializeField] string triggeringTag;
+    [SerializeField]
+    public int life = 3;
+    [SerializeField]
+     float respawn_time = 1f;
+    [SerializeField]
+    Vector3 start_position = new Vector3();
+    [SerializeField]
+    string lose_screen_name;
+
+
+
+    IEnumerator respawn()
+    {
+        gameObject.GetComponent<costume_controller>().enabled = false;
+        yield return new WaitForSeconds(respawn_time);
+        gameObject.GetComponent<costume_controller>().enabled = true;
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == triggeringTag && enabled)
+        if (other.tag == "enemy" && enabled)
         {
-            Destroy(this.gameObject);
+            life--;
+            if (life > 0)
+            {
+                gameObject.GetComponent<points_handler>().life_to_text(life);
+                transform.position = start_position;
+                StartCoroutine(respawn());
+            }
+            else
+            {
+                SceneManager.LoadScene(lose_screen_name);
+            }
+      
+        }
+        if (other.tag == "boundery" && enabled)
+        {
+            Destroy(other.gameObject);
         }
     }
 }
